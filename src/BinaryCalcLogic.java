394da9dc71366c0ba918f196fc178c3608464ea3
Jakob Lamb <jdlamb@bsu.edu>
2020-09-10 17:57:08 -0400

    Implemented square and squaroot functions plus tests for them

--- src/BinaryCalcLogic.java
+++ src/BinaryCalcLogic.java
@@ -5,5 +5,14 @@ public class BinaryCalcLogic {
     public static String toBinary(int decimalNum){
         return Integer.toBinaryString(decimalNum);
     }
-
+    public static int square(int decimalNum){
+        return Math.round(decimalNum * decimalNum);
+    }
+    public static int squareroot(int decimalNum){
+        if (decimalNum < 0){
+            return 0;
+        } else {
+            return (int) Math.round(Math.sqrt(decimalNum));
+        }
+    }
 }
--- test/LogicTester.java
+++ test/LogicTester.java
@@ -13,5 +13,34 @@ public class LogicTester {
         String result=BinaryCalcLogic.toBinary(15);
         assertEquals("1111",result);
     }
-
+    @Test
+    public void testnormalSquare(){
+        int result=BinaryCalcLogic.square(9);
+        assertEquals(81,result);
+    }
+    @Test
+    public void testnegativeSquare(){
+        int result=BinaryCalcLogic.square(-6);
+        assertEquals(36,result);
+    }
+    @Test
+    public void testzeroSquare(){
+        int result=BinaryCalcLogic.square(0);
+        assertEquals(0,result);
+    }
+    @Test
+    public void testnormalSquareroot(){
+        int result=BinaryCalcLogic.squareroot(81);
+        assertEquals(9,result);
+    }
+    @Test
+    public void testnegativeSquareroot(){
+        int result=BinaryCalcLogic.squareroot(-112);
+        assertEquals(0,result);
+    }
+    @Test
+    public void testzeroSquareroot(){
+        int result=BinaryCalcLogic.squareroot(0);
+        assertEquals(0,result);
+    }
 }
