7698697e7c937dce2db859979ebbf00ef0591172
nickd <nddagostino@bsu.ecu>
2020-09-10 20:00:19 -0400

    Fixed some merge errors and added negative coverage to the binary conversion

--- src/BinaryCalcLogic.java
+++ src/BinaryCalcLogic.java
@@ -1,25 +1,32 @@
 public class BinaryCalcLogic {
     public static int toDecimal(String binaryNum) {
         return Integer.parseInt(binaryNum,2);
+
+
     }
     public static String toBinary(int decimalNum){
-        return Integer.toBinaryString(decimalNum);
+        if (decimalNum>0){
+            return Integer.toBinaryString(decimalNum);
+        }else{
+            decimalNum=decimalNum*-1;
+            return "-"+Integer.toBinaryString(decimalNum);
+        }
     }
 
     public static int square(int decimalNum){
         return Math.round(decimalNum * decimalNum);
     }
-    public static int squareroot(int decimalNum){
-        if (decimalNum < 0){
+    public static int squareRoot(int decimalNum) {
+        if (decimalNum < 0) {
             return 0;
         } else {
             return (int) Math.round(Math.sqrt(decimalNum));
         }
-
-    public static int addition(int Num1, int Num2){
+    }
+    public static int addition (int Num1, int Num2){
         return Num1 + Num2;
     }
-    public static int subtraction(int Num1, int Num2){
+    public static int subtraction (int Num1, int Num2){
         return Num1 - Num2;
     }
 }
--- test/LogicTester.java
+++ test/LogicTester.java
@@ -9,38 +9,48 @@ public class LogicTester {
         assertEquals(15,result);
     }
     @Test
+    public void testToDecimalNegative(){
+        int result=BinaryCalcLogic.toDecimal("-1111");
+        assertEquals(-15,result);
+    }
+    @Test
     public void testToBinary(){
         String result=BinaryCalcLogic.toBinary(15);
         assertEquals("1111",result);
     }
     @Test
-    public void testnormalSquare(){
+    public void testToBinaryNegative(){
+        String result=BinaryCalcLogic.toBinary(-15);
+        assertEquals("-1111",result);
+    }
+    @Test
+    public void testNormalSquare(){
         int result=BinaryCalcLogic.square(9);
         assertEquals(81,result);
     }
     @Test
-    public void testnegativeSquare(){
+    public void testNegativeSquare(){
         int result=BinaryCalcLogic.square(-6);
         assertEquals(36,result);
     }
     @Test
-    public void testzeroSquare(){
+    public void testZeroSquare(){
         int result=BinaryCalcLogic.square(0);
         assertEquals(0,result);
     }
     @Test
-    public void testnormalSquareroot(){
-        int result=BinaryCalcLogic.squareroot(81);
+    public void testNormalSquareRoot(){
+        int result=BinaryCalcLogic.squareRoot(81);
         assertEquals(9,result);
     }
     @Test
-    public void testnegativeSquareroot(){
-        int result=BinaryCalcLogic.squareroot(-112);
+    public void testNegativeSquareRoot(){
+        int result=BinaryCalcLogic.squareRoot(-112);
         assertEquals(0,result);
     }
     @Test
-    public void testzeroSquareroot(){
-        int result=BinaryCalcLogic.squareroot(0);
+    public void testZeroSquareRoot(){
+        int result=BinaryCalcLogic.squareRoot(0);
         assertEquals(0,result);
     }
     @Test
