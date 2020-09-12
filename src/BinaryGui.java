a0981bdbba22e14820b600b7848f4795ac22e97c
Justin <JRussell3bsu@gmail.com>
2020-09-11 21:23:32 -0400

    Convert toBinary, and equals functionality added.

--- src/BinaryGui.java
+++ src/BinaryGui.java
@@ -9,7 +9,8 @@ public class BinaryGui extends JPanel {
         //construct components
         JButton binary0 = new JButton("0");
         JButton binary1 = new JButton("1");
-        JButton convert = new JButton("Convert");
+        JButton convertD = new JButton("ConvertD");
+        JButton convertB = new JButton("ConvertB");
         JLabel title = new JLabel("Binary Calculator");
         JTextField display = new JTextField(10);
         JButton plus = new JButton("+");
@@ -27,7 +28,8 @@ public class BinaryGui extends JPanel {
         //add components
         add (binary0);
         add (binary1);
-        add (convert);
+        add (convertD);
+        add (convertB);
         add (title);
         add (display);
         add (plus);
@@ -41,7 +43,8 @@ public class BinaryGui extends JPanel {
         //set component bounds (only needed by Absolute Positioning)
         binary0.setBounds (25, 60, 50, 25);
         binary1.setBounds (85, 60, 50, 25);
-        convert.setBounds (60, 155, 95, 25);
+        convertD.setBounds (15, 155, 90, 25);
+        convertB.setBounds (105, 155, 90, 25);
         title.setBounds (60, 5, 100, 25);
         display.setBounds (60, 30, 100, 25);
         plus.setBounds (25, 90, 50, 25);
@@ -93,13 +96,43 @@ public class BinaryGui extends JPanel {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String contents = display.getText();
+                String[] arrOfConts = contents.split(" ", 2);
+                if (arrOfConts [1] == "+" ){
+                    int Num1=((BinaryCalcLogic.toDecimal(arrOfConts [0])));
+                    int Num2=((BinaryCalcLogic.toDecimal(arrOfConts [2])));
+                    display.setText(Integer.toString(BinaryCalcLogic.addition(Num1, Num2)));
+                }
+                else if (arrOfConts [1] == "-" ){
+                    int Num1=((BinaryCalcLogic.toDecimal(arrOfConts [0])));
+                    int Num2=((BinaryCalcLogic.toDecimal(arrOfConts [2])));
+                    display.setText(Integer.toString(BinaryCalcLogic.subtraction(Num1, Num2)));
+                }
+                else if (arrOfConts [1] == "X" ){
+                    int Num1=((BinaryCalcLogic.toDecimal(arrOfConts [0])));
+                    int Num2=((BinaryCalcLogic.toDecimal(arrOfConts [2])));
+                    display.setText(Integer.toString(BinaryCalcLogic.multiplication(Num1, Num2)));
+                }
+                else if (arrOfConts [1] == "÷" ){
+                    int Num1=((BinaryCalcLogic.toDecimal(arrOfConts [0])));
+                    int Num2=((BinaryCalcLogic.toDecimal(arrOfConts [2])));
+                    display.setText(Integer.toString(BinaryCalcLogic.division(Num1, Num2)));
+                }
+
             }
         });
-        convert.addActionListener(new ActionListener() {
+        convertD.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
                 String contents = display.getText();
                 display.setText(Integer.toString(BinaryCalcLogic.toDecimal(contents)));
+            }
+        });
+        convertB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String contents = display.getText();
+                int i=Integer.parseInt(contents);
+                display.setText(BinaryCalcLogic.toBinary(i));
             }
         });
         exponent.addActionListener(new ActionListener() {
